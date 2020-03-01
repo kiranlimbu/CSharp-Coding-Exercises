ff31e3827b08efb971622f304af1403b2588e8ce
Kiran Limbu <58098835+kiranlimbu@users.noreply.github.com>
2020-03-01 11:19:33 -0800

    Add files via upload
    1. Break a given integer
    2. Add them
    3. If the sum is two digit or more, Repeat steps 1 and 2
    4. Return single digit sum

---
+++ SingleDigitSum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace singleDigitSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* break given integer, add them, if sum is single digit return esle break again */
+            Console.WriteLine(string.Join(" ", singleDigitSum(0)));
+        }
+
+        public static int singleDigitSum(int x)
+        {
+            int sum = 0;
+            int check = 10;
+
+            while (check > 9)
+            {
+                sum = 0;
+                while (x > 0)
+                {
+                    sum = x % 10 + sum;
+                    x = x / 10;
+                }
+                x = sum;
+                check = sum;
+            }
+            return sum;
+
+        }
+    }
+}
