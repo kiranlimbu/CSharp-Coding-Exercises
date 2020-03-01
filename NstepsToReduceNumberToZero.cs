3efec8a476464b0239ce7793884a7ec6fa91f4f2
Kiran Limbu <58098835+kiranlimbu@users.noreply.github.com>
2020-03-01 11:41:30 -0800

    Add files via upload
    use counter to count each step
    1. If even, divide by 2
    2. If odd, substract 1
    3. Continue step 1 and 2, until reduce to 0
    4. Return count

---
+++ NstepsToReduceNumberToZero.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ReduceNumberToZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = NumberOfSteps(123);
+            Console.WriteLine(result.ToString());
+
+        }
+        public static int NumberOfSteps(int num)
+        {
+            int count = 0;
+
+            while (num > 0)
+            {
+                if (num % 2 == 0)
+                {
+                    num = num / 2;
+                    count += 1;
+                }
+                else
+                {
+                    num = num - 1;
+                    count += 1;
+                }
+            }
+            return count;
+
+        }
+    }
+}
