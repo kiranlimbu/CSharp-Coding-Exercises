2fc74321e8ab56219e56359ce4ae5842fb95aff0
Kiran Limbu <58098835+kiranlimbu@users.noreply.github.com>
2020-03-10 09:10:51 -0700

    Number of Steps to Reduce a Number to Zero
    Given a non-negative integer num, return the number of steps to reduce it to zero. If the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
    
     
    
    Example:
    
    Input: num = 14
    Output: 6
    Explanation: 
    Step 1) 14 is even; divide by 2 and obtain 7. 
    Step 2) 7 is odd; subtract 1 and obtain 6.
    Step 3) 6 is even; divide by 2 and obtain 3. 
    Step 4) 3 is odd; subtract 1 and obtain 2. 
    Step 5) 2 is even; divide by 2 and obtain 1. 
    Step 6) 1 is odd; subtract 1 and obtain 0.

---
+++ ReduceNumberToZero.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ReduceNumberToZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = NumberOfSteps(123);
+            Console.WriteLine(result.ToString());
+
+        }
+        public static int NumberOfSteps(int num)
+        {
+            int count = 0;
+
+            while (num > 0)
+            {
+                // if even
+                if (num % 2 == 0)
+                {
+                    // divide the number by 2
+                    num /= 2;
+                    count += 1;
+                }
+                // if odd
+                else
+                {
+                    // substract 1
+                    num -= 1;
+                    count += 1;
+                }
+            }
+            // return total number of steps
+            return count;
+
+        }
+    }
+}
