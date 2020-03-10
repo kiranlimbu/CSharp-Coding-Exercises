46c79e862ad51d86accbd2c7b8c74ca6527fec75
Kiran Limbu <58098835+kiranlimbu@users.noreply.github.com>
2020-03-10 09:50:38 -0700

    Single Number
    Given a non-empty array of integers, every element appears twice except for one. Find that single one.
    
    Note:
    
    Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
    
    Example 1:
    Input: [2,2,1]
    Output: 1

---
+++ SingleNumber.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SingleInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] given = {1,2,2,3,4,3,4,5,5};
+            int res = SingleNumber2(given);
+            Console.WriteLine(res);
+        }
+        public static int SingleNumber1(int[] nums)
+        {
+            // sort the array
+            Array.Sort(nums);
+
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                // check current value with next value
+                if (nums[i] != nums[i + 1])
+                {
+                    // if not same, return the value
+                    return nums[i];
+                }
+                // else move two step forward
+                else
+                {
+                    i += 1;
+                }
+            }
+            // return that last value
+            return nums[^1];
+        }
+
+        // Better Version
+        public static int SingleNumber2(int[] nums)
+        {
+            int result = 0;
+            // iterate through each value in array
+            foreach (int n in nums)
+            {
+                // using exclusive OR(^) operator store non dup value
+                result ^= n;
+            }
+            return result;
+        }
+    }
+}
