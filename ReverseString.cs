4fce6535047f4910f99f74d8385b3378a7cde801
Kiran Limbu <58098835+kiranlimbu@users.noreply.github.com>
2020-03-09 18:44:37 -0700

    Reverse String

---
+++ ReverseString.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LeetCode344
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] given = { 'h', 'e', 'l', 'l', 'o' };
+            ReverseString(given);
+        }
+        public static void ReverseString(char[] s)
+        {
+            // get the length of an array
+            int n = s.Length - 1;
+
+            for (int i = 0; i <= n; i++)
+            {
+                // store first letter in temp variable
+                char temp = s[i];
+                // replace first letter with last letter
+                s[i] = s[n];
+                // replace last letter with first letter from temp
+                s[n] = temp;
+                // reduce length
+                n -= 1;
+            }
+            Console.WriteLine(s);
+        }
+    }
+}
