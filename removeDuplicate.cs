bc4b1e8e4ee3f4983994c11c4e64892c6181a389
Kiran Limbu <58098835+kiranlimbu@users.noreply.github.com>
2020-03-09 19:35:30 -0700

    Remove Duplicates from Sorted Array
    Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
    
    Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.

---
+++ removeDuplicate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+
+namespace removeDuplicate
+{
+    class RemoveDuplicate
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+            Console.WriteLine(RemoveDuplicates(arr));
+        }
+
+        public static int RemoveDuplicates(int[] nums)
+        {
+            int n = nums.Length;
+            int count = 1;
+            int curr = 0;
+            // if no element in the array, return 0
+            if (n < 1) { return 0; }
+
+            for (int i = 1; i < n; i++)
+            {
+                // if current is not equal to next
+                if (nums[curr] != nums[i])
+                {
+                    // then add the non dup to the array
+                    nums[count] = nums[i];
+                    // move current position to next
+                    curr = i;
+                    // add count
+                    count++;
+                }
+            }
+            // return length of unique numbers array
+            return count;
+        }
+    }
+}
