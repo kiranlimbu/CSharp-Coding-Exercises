eed424abdc58b2cf400fc95757ec82ba3eab22b1
Kiran Limbu <58098835+kiranlimbu@users.noreply.github.com>
2020-03-09 19:00:03 -0700

    Fizz Buzz
    Write a program that outputs the string representation of numbers from 1 to n.
    
    But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.

---
+++ FizzBuzz.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode412
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int given = 15;
+            Console.WriteLine(string.Join(",\n", FizzBuzz(given))); ;
+        }
+        public static IList<string> FizzBuzz(int n)
+        {
+            // initialize list<string>
+            var zap = new List<String>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                // if multiple of 3 and 5, add 'FizzBuzz'
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    zap.Add("FizzBuzz");
+                }
+                // if multiple of only 3, add 'Fizz'
+                else if (i % 3 == 0)
+                {
+                    zap.Add("Fizz");
+                }
+                // if multiple of only 5, add 'Buzz'
+                else if (i % 5 == 0)
+                {
+                    zap.Add("Buzz");
+                }
+                // else add the number
+                else
+                {
+                    zap.Add(i.ToString());
+                }
+            }
+            return zap;
+        }
+    }
+}
