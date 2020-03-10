6f59beca4a41e95c19e744f256de005f3a7f8e89
Kiran Limbu <58098835+kiranlimbu@users.noreply.github.com>
2020-03-10 12:19:05 -0700

    Reverse Integer
    Given a 32-bit signed integer, reverse digits of an integer.
    
    Example 1:
    Input: 123
    Output: 321
    
    Example 2:
    Input: -123
    Output: -321

---
+++ ReverseInteger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    class ReverseInteger
+    {
+        static void Main(string[] args)
+        {
+            int x = -140;
+            Console.WriteLine(reverse(x));
+
+        }
+
+        public static int reverse(int x)
+        {
+            int result = 0;
+            // loop until x is equal to 0
+            while (x != 0)
+            {
+                // get the remainder
+                int remainder = x % 10;
+                // truncate the integer
+                x /= 10;
+
+                try
+                {
+                    // flip the given integer
+                    result = checked(remainder + result * 10);
+                }
+                // if the integer is over int32, return 0
+                catch (System.OverflowException)
+                {
+                    return 0;
+                }
+                
+            }
+            return result;
+        }
+    }
+}
