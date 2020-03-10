179a9d0ddba488b127b12ba9f4dcce7a713c3213
Kiran Limbu <58098835+kiranlimbu@users.noreply.github.com>
2020-03-09 22:42:34 -0700

    Palindrome Number
    Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
    
    Example 1:
    
    Input: 121
    Output: true
    Example 2:
    
    Input: -121
    Output: false
    Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.

---
+++ PalindromeNumber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace LeetCode9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int given = 121;
+            Console.WriteLine(IsPalindrome(given));
+        }
+        public static bool IsPalindrome(int x)
+        {
+            // store original value of x
+            int givenInt = x;
+            int result = 0;
+
+            // if x is negative return false
+            // if x is greater then 0 but multiple of 10, return false
+            if (x < 0 || (x % 10 == 0 && x != 0))
+            {
+                return false;
+            }
+
+            while (x > 0)
+            {
+                // get the remainder + add zero to the back of result
+                // result = 1 + 0
+                // 1 = 2 + 10 and so on
+                result = x % 10 + result * 10;
+                // truncate the last number from the integer
+                // x = 121 / 10 = 12 and so on
+                x /= 10;
+            }
+            // if equal return true else false
+            return givenInt == result;
+        }
+    }
+}
