b643aede357b683d827d84a4b0799ecbb26f38d4
Kiran Limbu <58098835+kiranlimbu@users.noreply.github.com>
2020-03-10 18:38:12 -0700

    Happy Number
    Write an algorithm to determine if a number is "happy".
    
    A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
    
    Example: 
    Input: 19
    Output: true
    Explanation: 
    12 + 92 = 82
    82 + 22 = 68
    62 + 82 = 100
    12 + 02 + 02 = 1

---
+++ HappyNumber.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace LeetCode202
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int given = 19;
+            Console.WriteLine(IsHappy(given));
+        }
+
+        public static bool IsHappy(int n)
+        {
+            int sqrSum = 0;
+            HashSet<int> set = new HashSet<int>();
+
+            while (n > 0)
+            {
+                sqrSum = (n % 10)*(n % 10) + sqrSum;
+                n /= 10;
+
+                // if integer is completely broken
+                if (n == 0)
+                {
+                    // if the sum of sqr is 1 return true
+                    if (sqrSum == 1) return true;
+                    // replace n value with new sqr sum
+                    n = sqrSum;
+                    // helps break out of infinite loop
+                    // check if HashSet contains sum
+                    if (set.Contains(sqrSum)) break;
+                    // add sum to HashSet
+                    else
+                    {
+                        set.Add(sqrSum);
+                        // reset the value of sqrSum
+                        sqrSum = 0;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
