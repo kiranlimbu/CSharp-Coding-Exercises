91eb0abc8733ea0ecf2c48e6230e0853df47ea09
Kiran Limbu <58098835+kiranlimbu@users.noreply.github.com>
2020-03-11 20:45:09 -0700

    Selection Sort
    Selection sort is a sorting algorithm, specifically an in-place comparison sort. It selects the smallest unsorted item remaining in the list. Then swapping it with the item in the next position to be filled.

---
+++ SelectionSort.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 5, -5, 7, 8, 0, 3, 6, 1 };
+            SelectorSort(arr);
+
+            Console.ReadLine();
+        }
+        static void SelectorSort(int[] lst)
+        {
+            int n = lst.Length;
+            int i, j, small, temp;
+
+            for (i = 0; i < n; i++)
+            {
+                small = lst[i];
+
+                for (j = i+1; j < n; j++)
+                {
+                    if (small > lst[j]) // if small is bigger then current number
+                    {
+                        small = lst[j]; // fill small value with current number
+                        temp = lst[i]; // put i value in temp
+                        lst[i] = small; // fill i with new found small
+                        lst[j] = temp; // fill j with old i value
+                    }
+                }  
+            }
+
+            foreach (int item in lst)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
