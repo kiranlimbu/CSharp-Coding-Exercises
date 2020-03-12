8abbf9e41013c7f4dd2d12e488eb3e495af1e474
Kiran Limbu <58098835+kiranlimbu@users.noreply.github.com>
2020-03-11 19:53:15 -0700

    Valid Palindrome
    Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
    
    Note: For the purpose of this problem, we define empty string as valid palindrome.
    
    Example 1:
    Input: "A man, a plan, a canal: Panama"
    Output: true

---
+++ ValidPalindrome.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Threading;
+
+namespace LeetCode125
+{
+    class ValidPalindrome
+    {
+        static void Main(string[] args)
+        {
+            string given = "A man, a plan, a canal: Panama";
+            Console.WriteLine(IsPalindrome1(given));
+        }
+        public static bool IsPalindrome(string s)
+        {
+
+            string phrase = s.ToLower();
+            string alphabet = "abcdefghijklmnopqrstuvwxyz01234556789";
+            string characters = "";
+
+            if (phrase.Length < 2) { return true; }
+            // only accepts alphabet and digits
+            for (int i = 0; i < phrase.Length; i++)
+            {
+                if (alphabet.Contains(phrase[i]))
+                {
+                    characters += phrase[i];
+                }
+            }
+            // check if first and last value match
+            int n = characters.Length;
+            for (int i = 0; i < n; i++)
+            {
+                if (characters[i] != characters[n - 1])
+                {
+                    return false;
+                }
+                else
+                {
+                    n -= 1;
+                }
+            }
+            return true;
+        }
+        // faster version
+        public static bool IsPalindrome1(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return true;
+            // front pointer
+            int start = 0;
+            // back pointer
+            int end = s.Length - 1;
+            while (start <= end)
+            {
+                // from front, separate alphabets/digits
+                while (start <= end && !Char.IsLetterOrDigit(s[start]))
+                {
+                    start++;
+                }
+                // from back, separate alphabets/digits
+                while (end >= start && !Char.IsLetterOrDigit(s[end]))
+                {
+                    end--;
+                }
+                Console.WriteLine("start: {0}", start);
+                Console.WriteLine(Char.IsLetterOrDigit(s[start]));
+                Thread.Sleep(2000);
+
+                // check first and last value and move inword
+                if (start <= end && Char.ToLower(s[start]) != Char.ToLower(s[end]))
+                {
+                    return false;
+                }
+                // move inword
+                start++;
+                end--;
+            }
+            return true;
+        }
+    }
+}
