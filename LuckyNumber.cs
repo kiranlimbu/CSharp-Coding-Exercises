74770591e7d9fc7cb924e429c79629666477745e
Kiran Limbu <58098835+kiranlimbu@users.noreply.github.com>
2020-03-15 13:21:48 -0700

    Lucky Numbers in a Matrix
    Given a m * n matrix of distinct numbers, return all lucky numbers in the matrix in any order.
    
    A lucky number is an element of the matrix such that it is the minimum element in its row and maximum in its column.
    
     
    
    Example 1:
    
    Input: matrix = [[3,7,8],[9,11,13],[15,16,17]]
    Output: [15]
    Explanation: 15 is the only lucky number since it is the minimum in its row and the maximum in its column

---
+++ LuckyNumber.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuckyNumbersMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] given = {
+
+                new int[] {1,10, 4, 2},
+                new int[] {9,3,8,7},
+                new int[] {15,16,17,12}
+
+            };
+
+            Console.WriteLine(string.Join(" ", LuckyNumbers(given)));
+
+            Console.ReadLine();
+        }
+        public static IList<int> LuckyNumbers(int[][] matrix)
+        {
+            List<int> maxCol = new List<int>() { };
+            List<int> lucky = new List<int>() { };
+
+            // find max in the column
+            for (int col = 0; col < matrix[0].Length; col++)
+            {
+                int x = 0;
+                for (int row = 0; row < matrix.Length; row++)
+                {
+                    x = Math.Max(matrix[row][col], x);
+                }
+                maxCol.Add(x);
+            }
+
+            // find min in the row
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                if (maxCol.Contains(matrix[i].Min()))
+                {
+                    lucky.Add(matrix[i].Min());
+                }
+            }
+            return lucky;
+
+        }
+    }
+}
