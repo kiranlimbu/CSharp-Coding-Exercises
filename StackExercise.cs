41908b80d688e765ecadf2ae738801f446f65bb6
Kiran Limbu <58098835+kiranlimbu@users.noreply.github.com>
2020-03-18 21:16:59 -0700

    Min Stack
    Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
    
    push(x) -- Push element x onto stack.
    pop() -- Removes the element on top of the stack.
    top() -- Get the top element.
    getMin() -- Retrieve the minimum element in the stack.
     
    
    Example:
    
    MinStack minStack = new MinStack();
    minStack.push(-2);
    minStack.push(0);
    minStack.push(-3);
    minStack.getMin();   --> Returns -3.
    minStack.pop();
    minStack.top();      --> Returns 0.
    minStack.getMin();   --> Returns -2.

---
+++ StackExercise.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode155
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MinStack myStack = new MinStack();
+            myStack.Push(2);
+            myStack.Push(5);
+            myStack.Push(3);
+            myStack.Push(1);
+
+            int minVal = myStack.GetMin();
+            Console.WriteLine(minVal);
+
+        }
+
+        public class MinStack
+        {
+            /** initialize your data structure here. */
+            LinkedList<int> obj;
+            public MinStack()
+            {
+                obj = new LinkedList<int>();
+            }
+
+            public void Push(int x)
+            {
+                obj.AddFirst(x);
+            }
+
+            public void Pop()
+            {
+                if (obj.Any()) // checks if the sequence has any element
+                {
+                    obj.RemoveFirst();
+                }
+                else
+                {
+                    throw new Exception("Stack is empty");
+                }
+                
+            }
+
+            // Peek
+            public int Top()
+            {
+                if (obj.Any())
+                {
+                    return obj.First();
+                }
+                else
+                {
+                    return 0;
+                }
+                
+            }
+
+            public int GetMin()
+            {
+                if (!obj.Any()) return 0;
+
+                int num = obj.First.Value;
+                foreach(int item in obj)
+                {
+                    if(item < num)
+                    {
+                        num = item;
+                    }
+                    
+                }
+                return num;
+            }
+        }
+
+    }
+}
