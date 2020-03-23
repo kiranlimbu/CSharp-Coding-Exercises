ee059b9647c830328e916794f55f06aab4e45290
Kiran Limbu <58098835+kiranlimbu@users.noreply.github.com>
2020-03-23 15:54:26 -0700

    Create Target Array in the Given Order
    Given two arrays of integers nums and index. Your task is to create target array under the following rules:
    
    Initially target array is empty.
    From left to right read nums[i] and index[i], insert at index index[i] the value nums[i] in target array.
    Repeat the previous step until there are no elements to read in nums and index.
    Return the target array.
    
    It is guaranteed that the insertion operations will be valid.
    
     
    
    Example 1:
    
    Input: nums = [0,1,2,3,4], index = [0,1,2,2,1]
    Output: [0,4,1,3,2]
    Explanation:
    nums       index     target
    0            0        [0]
    1            1        [0,1]
    2            2        [0,1,2]
    3            2        [0,1,3,2]
    4            1        [0,4,1,3,2]
    Example 2:
    
    Input: nums = [1,2,3,4,0], index = [0,1,2,3,0]
    Output: [0,1,2,3,4]
    Explanation:

---
+++ ShiftingValueInArray.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode1389
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 4, 2, 4, 3, 2 };
+            int[] index = { 0, 0, 1, 3, 1 };
+
+            Console.WriteLine(string.Join(" ", CreateTargetArray(nums, index)));
+        }
+
+        // for this problem this does not work
+        // this only works when inbetween indexs are not missing
+        public static int[] CreateTargetArray(int[] nums, int[] index)
+        {
+            int[] target = new int[index.Length];
+            int[] valid = new int[index.Length];
+
+            for (int i = 0; i < index.Length; i++)
+            {
+                // if the index number exist then do following
+                if (Array.Exists(valid, x => x == index[i]) && i != 0)
+                {
+                    int count = index[i];
+                    int current = target[count];
+                    while (count < i)
+                    {
+                        int temp = target[count + 1];
+                        target[count + 1] = current;
+                        current = temp;
+                        count++;
+                    }
+                }
+                target[index[i]] = nums[i];
+                valid[i] = index[i];
+            }
+            return target;
+        }
+
+        public static int[] CreateTargetArray(int[] nums, int[] index)
+        {
+            List<int> target = new List<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                target.Insert(index[i], nums[i]);
+            }
+            
+            return target.ToArray();
+        }
+    }
+}
