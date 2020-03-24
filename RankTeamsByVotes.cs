d0e88f6127bf4b8aa31a8f8d881314073e7ddb2b
Kiran Limbu <58098835+kiranlimbu@users.noreply.github.com>
2020-03-23 23:29:59 -0700

    Rank Teams by Votes
    In a special ranking system, each voter gives a rank from highest to lowest to all teams participated in the competition.
    
    The ordering of teams is decided by who received the most position-one votes. If two or more teams tie in the first position, we consider the second position to resolve the conflict, if they tie again, we continue this process until the ties are resolved. If two or more teams are still tied after considering all positions, we rank them alphabetically based on their team letter.
    
    Given an array of strings votes which is the votes of all voters in the ranking systems. Sort all teams according to the ranking system described above.
    
    Return a string of all teams sorted by the ranking system.
    
     
    
    Example 1:
    
    Input: votes = ["ABC","ACB","ABC","ACB","ACB"]
    Output: "ACB"
    Explanation: Team A was ranked first place by 5 voters. No other team was voted as first place so team A is the first team.
    Team B was ranked second by 2 voters and was ranked third by 3 voters.
    Team C was ranked second by 3 voters and was ranked third by 2 voters.
    As most of the voters ranked C second, team C is the second team and team B is the third.
    Example 2:
    
    Input: votes = ["WXYZ","XYZW"]
    Output: "XWYZ"
    Explanation: X is the winner due to tie-breaking rule. X has same votes as W for the first position but X has one vote as second position while W doesn't have any votes as second position. 
    Example 3:
    
    Input: votes = ["ZMNAGUEDSJYLBOPHRQICWFXTVK"]
    Output: "ZMNAGUEDSJYLBOPHRQICWFXTVK"
    Explanation: Only one voter so his votes are used for the ranking.
    Example 4:
    
    Input: votes = ["BCA","CAB","CBA","ABC","ACB","BAC"]
    Output: "ABC"
    Explanation: 
    Team A was ranked first by 2 voters, second by 2 voters and third by 2 voters.
    Team B was ranked first by 2 voters, second by 2 voters and third by 2 voters.
    Team C was ranked first by 2 voters, second by 2 voters and third by 2 voters.
    There is a tie and we rank teams ascending by their IDs.

---
+++ RankTeamsByVotes.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RankTeamsByVote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] given = { "WXYZ", "XYZW" };
+            Console.WriteLine(string.Join(" ", RankTeams(given)));
+        }
+
+        public static string RankTeams(string[] votes)
+        {
+
+            // separate element from array
+            Dictionary<char, int[]> character = new Dictionary<char, int[]>();
+            // initialize array for each character
+            foreach (char c in votes[0])
+            {
+                character[c] = new int[votes[0].Length];
+            }
+            
+            // Add/count number of times the team appear in that position
+            foreach (string vote in votes)
+            {
+                for (int i= 0; i<vote.Length; i++)
+                {
+                    character[vote[i]][i]++;
+                }
+            }
+
+            var lst = character.ToList(); // store[w, [1,0,0,1]] [x, [1,1,0,0]] [y, [0,1,1,0]] [z, [0,0,1,1]]
+
+            // sort List
+            lst.Sort((a, b) =>
+            {
+                for (int i = 0; i < a.Value.Length; i++)
+                {
+                    // compare two character at a time
+                    // if value is equal more to next column
+                    // pick the character with higher value
+                    if (a.Value[i] != b.Value[i]) // a b changes from x w, y w, z y 
+                    {
+                        return b.Value[i] - a.Value[i]; // returns -1, 1, 1                     
+                    }
+                }
+                return a.Key - b.Key;
+            });
+
+            // at this point List is sorted
+            char[] arr = lst.Select(a => a.Key).ToArray();
+            return new string(arr);
+        }
+    }
+}
