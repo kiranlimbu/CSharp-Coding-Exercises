737f42f4cf3c8b0b546efa8929ffbfb3a21c7de7
Kiran Limbu <58098835+kiranlimbu@users.noreply.github.com>
2020-04-05 12:35:29 -0700

    Valid Parentheses
    Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
    
    An input string is valid if:
    
    Open brackets must be closed by the same type of brackets.
    Open brackets must be closed in the correct order.
    Note that an empty string is also considered valid.
    
    Example 1:
    
    Input: "()"
    Output: true
    Example 2:
    
    Input: "()[]{}"
    Output: true
    Example 3:
    
    Input: "(]"
    Output: false

---
+++ ValidParentheses.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string given = "(]";
+            Console.WriteLine(IsValid(given));
+        }
+
+        public static bool IsValid(string s)
+        {
+            Stack<char> symbol = new Stack<char>();
+
+            foreach(var item in s)
+            {
+                if (item == '[' || item == '{' || item == '(')
+                {
+                    symbol.Push(item);
+                }
+                else if (symbol.Count != 0)
+                {
+                    if (item == ']' && symbol.Peek() == '[') symbol.Pop();
+                    else if (item == '}' && symbol.Peek() == '{') symbol.Pop();
+                    else if (item == ')' && symbol.Peek() == '(') symbol.Pop();
+                    return false;
+                }
+                else return false;
+            }
+            return symbol.Count == 0;
+        }
+    }
+}
