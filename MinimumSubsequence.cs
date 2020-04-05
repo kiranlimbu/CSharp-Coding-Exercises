13c63305da2416be6b7aee5852766735dac4ef73
Kiran Limbu <58098835+kiranlimbu@users.noreply.github.com>
2020-04-04 23:52:09 -0700

    Minimum Subsequence in Non-Increasing Order
    Given the array nums, obtain a subsequence of the array whose sum of elements is strictly greater than the sum of the non included elements in such subsequence. 
    
    If there are multiple solutions, return the subsequence with minimum size and if there still exist multiple solutions, return the subsequence with the maximum total sum of all its elements. A subsequence of an array can be obtained by erasing some (possibly zero) elements from the array. 
    
    Note that the solution with the given constraints is guaranteed to be unique. Also return the answer sorted in non-increasing order.
    
     
    
    Example 1:
    
    Input: nums = [4,3,10,9,8]
    Output: [10,9] 
    Explanation: The subsequences [10,9] and [10,8] are minimal such that the sum of their elements is strictly greater than the sum of elements not included, however, the subsequence [10,9] has the maximum total sum of its elements. 
    Example 2:
    
    Input: nums = [4,4,7,6,7]
    Output: [7,7,6] 
    Explanation: The subsequence [7,7] has the sum of its elements equal to 14 which is not strictly greater than the sum of elements not included (14 = 4 + 4 + 6). Therefore, the subsequence [7,6,7] is the minimal satisfying the conditions. Note the subsequence has to returned in non-decreasing order.

---
+++ MinimumSubsequence.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4April2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 4,3,10,9,8 };
+            var result = MinSubsequence(nums);
+            Console.WriteLine(string.Join(" ", result));
+        }
+
+        public static IList<int> MinSubsequence(int[] nums)
+        {
+            List<int> minSub = new List<int>();
+
+            if (nums.Max() > nums.Sum() || nums.Length == 1)
+            {
+                minSub.Add(nums.Max());
+                return minSub;
+            }
+
+            int idx = 0;
+            while (idx < nums.Length)
+            {
+                int bigNum = nums.Max();
+                minSub.Add(bigNum);             
+
+                // big number in the front
+                nums[Array.IndexOf(nums, bigNum)] = 0;
+
+                if (minSub.Sum() > nums.Sum()) return minSub;
+
+                idx++;
+            }
+
+            return minSub;
+        }
+    }
+}
