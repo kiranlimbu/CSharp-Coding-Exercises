474f4ecb80a4dbfa6e9037f0a1da3cef6af14783
Kiran Limbu <58098835+kiranlimbu@users.noreply.github.com>
2020-04-13 15:20:38 -0700

    String Matching in an Array
    Given an array of string words. Return all strings in words which is substring of another word in any order. 
    
    String words[i] is substring of words[j], if can be obtained removing some characters to left and/or right side of words[j].
    
     
    
    Example 1:
    
    Input: words = ["mass","as","hero","superhero"]
    Output: ["as","hero"]
    Explanation: "as" is substring of "mass" and "hero" is substring of "superhero".
    ["hero","as"] is also a valid answer.
    Example 2:
    
    Input: words = ["leetcode","et","code"]
    Output: ["et","code"]
    Explanation: "et", "code" are substring of "leetcode".
    Example 3:
    
    Input: words = ["blue","green","bu"]
    Output: []

---
+++ StringMatchingInArray.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode5380
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = { "leetcode", "et", "code" };
+            IList<string> hello = StringMatching(words);
+
+            foreach (var item in hello)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+
+        public static IList<string> StringMatching(string[] words)
+        {
+            List<string> match = new List<string>();
+            words = words.OrderBy(x => x.Length).ToArray();
+
+            int n = words.Length;
+
+            for (int i=0; i<n; i++)
+            {
+                for (int j=i+1; j<n; j++)
+                {
+                    if (words[j].Contains(words[i]))
+                    {
+                        match.Add(words[i]);
+                    }                  
+                }
+            }
+            return match;
+        }
+    }
+}
